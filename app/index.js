eb3aa36feeb7a58bdf4bed3213d397d9dd525952
Noziii <100632745+Noziii@users.noreply.github.com>
2023-11-17 04:46:50 +0200

    final commit

--- app/index.js
+++ app/index.js
@@ -5,7 +5,11 @@ import Home from "./Home";
 import CampusEvents from "./CampusEvents";
 import Res from "./Res";
 import LocalHotspots from "./LocalHotspots";
-import "expo-router/entry";
+import HomeScreen from "../matrixgo/screens/HomeScreen";
+import RestaurantScreen from "../matrixgo/screens/RestaurantScreen";
+import CartScreen from "../matrixgo/screens/CartScreen";
+import OrderPreparingScreen from "../matrixgo/screens/OrderPreparingScreen";
+import DeliveryScreen from "../matrixgo/screens/DeliveryScreen";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +20,23 @@ const App = () => {
       <Stack.Screen name="CampusEvents" component={CampusEvents} />
       <Stack.Screen name="Res" component={Res} />
       <Stack.Screen name="LocalHotspots" component={LocalHotspots} />
+      <Stack.Screen name="MatrixGo" component={HomeScreen} />
+      <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+      <Stack.Screen
+        name="Cart"
+        options={{ presentation: "modal" }}
+        component={CartScreen}
+      />
+      <Stack.Screen
+        name="Order Received"
+        options={{ presentation: "fullScreenModal" }}
+        component={OrderPreparingScreen}
+      />
+      <Stack.Screen
+        name="Preparing Order"
+        options={{ presentation: "fullScreenModal" }}
+        component={DeliveryScreen}
+      />
     </Stack.Navigator>
   );
 };
